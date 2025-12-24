63bd18297261b3a2d3e9eee55eb21cdf864bd367
Zuhair Zia <zuhairzia1@gmail.com>
2025-12-24 20:31:09 +0500

    Update MongoDB URI for production environment

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb://localhost:27017/BusTicketSystem"
+    MongoURI: "mongodb+srv://zuhairzia1_db_user:lACgFQHlBfgbNgBO@cluster0.80xfcer.mongodb.net/?appName=Cluster0"
 }
